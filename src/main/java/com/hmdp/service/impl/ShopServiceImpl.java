122d46bb752f9e7e0d2327153147e42993f048a1
京颖 杜 <1095168498@qq.com>
2025-01-16 23:56:53 +0800

    给查询商户的缓存添加超时剔除和主动更新策略-20250116

--- src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
@@ -9,8 +9,11 @@ import com.hmdp.service.IShopService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
+
+import java.util.concurrent.TimeUnit;
 
 import static com.hmdp.utils.RedisConstants.*;
 
@@ -30,8 +33,10 @@ public class ShopServiceImpl extends ServiceImpl<ShopMapper, Shop> implements IS
     @Override
     public Result queryById(Long id) {
         String key = CACHE_SHOP_KEY + id;
+
         //在redis中查询商户
         String shopJson = stringRedisTemplate.opsForValue().get(key);
+
         //存在，则返回
         if (StrUtil.isNotBlank(shopJson)){
             Shop shop = JSONUtil.toBean(shopJson, Shop.class);
@@ -43,9 +48,28 @@ public class ShopServiceImpl extends ServiceImpl<ShopMapper, Shop> implements IS
         if (shop == null){
             return Result.fail("商户不存在");
         }
-        //存在，将查询的数据放到redis中
-        stringRedisTemplate.opsForValue().set(key,JSONUtil.toJsonStr(shop));
+
+        // 存在，将查询的数据放到redis中
+        // 并同时设置 redis 缓存的过期时间
+        stringRedisTemplate.opsForValue().set(key,JSONUtil.toJsonStr(shop),CACHE_SHOP_TTL, TimeUnit.MINUTES);
+
         //返回信息
         return Result.ok(shop);
+    }
+
+    @Override
+    @Transactional
+    public Result update(Shop shop) {
+        Long id = shop.getId();
+        if (id==null)return Result.fail("商户id为空");
+
+        // 这里先“更新数据库”再“删除缓存”，降低多线程读缓存时出现缓存与数据库数据不一致的问题。
+        // 更新数据库
+        updateById(shop);
+
+        // 删除缓存
+        stringRedisTemplate.delete(CACHE_SHOP_KEY+id);
+
+        return Result.ok();
     }
 }
